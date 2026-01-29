bfbf069122f72c1ff7f54491b2f9af4fe190943c
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2026-01-29 13:17:39 -0500

    clase excepciones y jacoco,sonar funcionando

---
+++ src/main/java/edu/eci/arsw/parallelism/api/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package edu.eci.arsw.parallelism.api;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolation(ConstraintViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+}
